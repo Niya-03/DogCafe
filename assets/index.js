8e0b6466fc60382260e0823141b0ff09e9894843
dianasim <diana.simeonova56@gmail.com>
2025-11-29 23:08:23 +0200

    swiper js setup

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,14 @@
-function loadHTML(id, url) {
-  fetch(url, { cache: "force-cache" })
-    .then(response => response.text())
-    .then(data => document.getElementById(id).innerHTML = data);
-}
+import Swiper from "../node_modules/swiper/swiper-bundle.min.mjs";
 
-loadHTML("header", "header.html");
-loadHTML("footer", "footer.html");
+document.addEventListener("DOMContentLoaded", () => {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        centeredSlides: false,
+        loop: false,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+})

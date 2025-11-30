25079e753b8322e58493a1b255780eda3507f1a7
dianasim <diana.simeonova56@gmail.com>
2025-11-30 21:24:43 +0200

    separate header footer into files and load them with js

--- assets/index.js
+++ assets/index.js
@@ -4,11 +4,25 @@ document.addEventListener("DOMContentLoaded", () => {
     const swiper = new Swiper('.swiper', {
         slidesPerView: 'auto',
         spaceBetween: 20,
-        centeredSlides: false,
-        loop: false,
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
     });
 })
+
+async function loadHTML (id, file) {
+    const container = document.getElementById(id);
+    if (!container) return;
+
+    try {
+        const response = await fetch(file);
+        const html = await response.text();
+        container.innerHTML = html;
+    } catch (error) {
+        console.error("Error loading html:", file, error);
+    }
+}
+
+loadHTML("headerContainer", "/partials/header.html");
+loadHTML("footerContainer", "/partials/footer.html");

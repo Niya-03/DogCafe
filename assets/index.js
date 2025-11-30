68b9c14eddf2c37988326438673dcc91bff4ecbf
Diana Simeonova <122699903+dianasimeonova56@users.noreply.github.com>
2025-11-30 22:33:02 +0200

    Diana (#6)
    * rearrange project, install sass, add pictures
    
    * commit
    
    * add scss, ReadMe.md
    
    * add base index html with examples for scss, add fonts
    
    * add border scss, add header, footer modifications
    
    * hero section
    
    * slight change on font
    
    * fix border scc
    
    * add swiper
    
    * add/remove imgs
    
    * swiper js setup
    
    * add slider, about us, menu page, fix hero with 2 images for desktop/mobile
    
    * separate header footer into files and load them with js
    
    * add reveal animation
    
    * change textx
    
    * add marquee, fix forms

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,44 @@
-function loadHTML(id, url) {
-  fetch(url, { cache: "force-cache" })
-    .then(response => response.text())
-    .then(data => document.getElementById(id).innerHTML = data);
+import Swiper from "../node_modules/swiper/swiper-bundle.min.mjs";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+})
+
+async function loadHTML(id, file) {
+    const container = document.querySelectorAll(`#${id}`);
+    if (!container) return;
+
+    try {
+        const response = await fetch(file);
+        const html = await response.text();
+        container.forEach(c => {
+            c.innerHTML = html;
+        })
+
+    } catch (error) {
+        console.error("Error loading html:", file, error);
+    }
 }
 
-loadHTML("header", "header.html");
-loadHTML("footer", "footer.html");
+loadHTML("headerContainer", "/partials/header.html");
+loadHTML("footerContainer", "/partials/footer.html");
+loadHTML("marqueeContainer", "/partials/marquee.html");
+
+const sections = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(sections => {
+    sections.forEach(section => {
+        if (section.isIntersecting) {
+            section.target.classList.add("active");
+        }
+    });
+});
+
+sections.forEach(section => observer.observe(section));
